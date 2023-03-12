3af168438b94ee060d2b4e09941f8f97a9fcdd5f
rsugaya <rsugaya@llc-noa.net>
2023-03-12 16:06:03 +0900

    Initial Commit

---
+++ HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by 菅谷亮太 on 2023/03/12.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
