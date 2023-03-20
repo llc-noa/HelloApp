7b5067fa6ee7764cc43228c46d5fb927a311e442
rsugaya <rsugaya@llc-noa.net>
2023-03-20 17:18:33 +0900

    コミット

--- HelloApp/ContentView.swift
+++ HelloApp/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var outputText = "Hello, world!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(outputText)
+                .font(.largeTitle)
+            
+            Button("切り替えボタン") {
+                outputText = "Hi Swift"
+            }
+            .padding(.all)
+            .background(Color.blue)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
         }
-        .padding()
     }
 }
 
